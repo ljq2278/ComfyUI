cb30bcb245b94bd4171ddcf7a03af4c8291493e4
ljq2278 <ljq2278@163.com>
2025-06-01 14:46:15 +0800

    update

--- my_workspace/api_task/api_get_res_t2v.py
+++ my_workspace/api_task/api_get_res_t2v.py
@@ -8,10 +8,10 @@ from my_workspace.materials.bg_shots import mv_shots_no_protagonist
 
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
-
-COMFYUI_URL = "http://127.0.0.1:8190"  # ComfyUI API 地址
-WORKFLOW_API_JSON_FILE = "/workspace/ComfyUI/my_workspace/comfy重要工作流/文字生视频_api.json"  # 你的工作流API格式文件
-OUTPUT_VIDEO_DIR = "/workspace/ComfyUI/output/bg_shots"
+COMFYUI_PATH = "f:/projects/ComfyUI"
+COMFYUI_URL = "http://127.0.0.1:8188"  # ComfyUI API 地址
+WORKFLOW_API_JSON_FILE = COMFYUI_PATH + "/my_workspace/comfy重要工作流/文字生视频_api.json"  # 你的工作流API格式文件
+OUTPUT_VIDEO_DIR = COMFYUI_PATH + "/output/bg_shots"
 
 
 def queue_prompt(prompt_workflow):
@@ -33,23 +33,22 @@ def set_workflow(current_workflow, **kwargs):
 
     save_video_node_id = "6"  # 替换为实际的节点ID
     current_workflow[save_video_node_id]["inputs"]["text"] = f"""{kwargs["prompt"]}"""
-    
+
     save_video_node_id = "7"  # 替换为实际的节点ID
     current_workflow[save_video_node_id]["inputs"]["text"] = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，倒着走"
-    
+
     save_video_node_id = "40"  # 替换为实际的节点ID
     current_workflow[save_video_node_id]["inputs"]["length"] = 33
-    
+
     save_video_node_id = "52"  # 替换为实际的节点ID
     current_workflow[save_video_node_id]["inputs"]["filename_prefix"] = os.path.join(OUTPUT_VIDEO_DIR, f"""res_{kwargs["exp_name"]}_{kwargs["mv_shot_ind"]}"""
-    )
-    
-    
+                                                                                     )
+
     return current_workflow
 
 
 # 加载工作流模板
-with open(WORKFLOW_API_JSON_FILE, 'r') as f:
+with open(WORKFLOW_API_JSON_FILE, 'r', encoding="utf-8") as f:
     base_workflow = json.load(f)
 
 nm = mv_shots_no_protagonist["name"]
